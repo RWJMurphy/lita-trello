2eda14a83489ec07d2e6ebcd6bd6cc06ffd8371e
Eric Sigler <me@esigler.com>
2015-09-29 18:47:39 -0700

    Move to localized strings

--- lib/lita/handlers/trello.rb
+++ lib/lita/handlers/trello.rb
@@ -8,7 +8,7 @@ module Lita
       config :board
 
       route(/^trello\s+new\s+[^\s]+\s+[^\s]+/i, :handle_create_card, command: true, help: {
-        "trello new LIST Do the thing" => "Create a new card in LIST called 'Do the thing'"
+        t('help.new.syntax') => t('help.new.desc')
       })
       def handle_create_card(r)
         list_name = r.args[1]
@@ -16,21 +16,21 @@ module Lita
 
         list_id = lists[list_name.downcase].id
         if list_id.nil?
-          r.reply "I couldn't find a list named #{list_name}."
+          r.reply t('error.no_list', list_name: list_name)
           return
         end
 
-        r.reply "Ok #{r.user.name}, I'm creating a new card in #{list_name}."
+        r.reply t('card.creating', user_name: r.user.name, list_name: list_name)
         begin
           card = new_card(name, list_id)
-          r.reply "Here you go: #{card.short_url}"
+          r.reply t('card.created', url: card.short_url)
         rescue
-          r.reply "Something failed."
+          r.reply t('error.generic')
         end
       end
 
       route(/^trello\s+move\s+[^\s]+\s+[^\s]+/i, :handle_move_card, command: true, help: {
-        "trello move https://trello.com/c/CARD_ID LIST" => "Move a card to LIST"
+        t('help.move.syntax') => t('help.move.desc')
       })
       def handle_move_card(r)
         card_id = r.args[1]
@@ -38,7 +38,7 @@ module Lita
 
         list = lists[list_name.downcase]
         if list.nil?
-          r.reply "I couldn't find a list named #{list_name}."
+          r.reply t('error.no_list', list_name: list_name)
           return
         end
 
@@ -49,39 +49,39 @@ module Lita
 
         card = trello.find(:card, card_id)
         if card.nil?
-          r.reply "I couldn't find that card."
+          r.reply t('error.no_card')
           return
         end
 
         begin
           card.move_to_list(list)
-          r.reply "Ok #{r.user.name}, I moved that card to #{list_name}."
+          r.reply t('card.moved', user_name: r.user.name, list_name: list_name)
         rescue
-          r.reply "Something failed."
+          r.reply t('error.generic')
         end
       end
 
       route(/^trello\s+list\s+[^\s]+/i, :handle_list_cards, command: true, help: {
-        "trello list LIST" => "Show all cards in LIST"
+        t('help.list.syntax') => t('help.list.desc')
       })
       def handle_list_cards(r)
         list_name = r.args[1]
         list = lists[list_name.downcase]
         if list.nil?
-          r.reply "I couldn't find a list named #{list_name}."
+          r.reply t('error.no_list', list_name: list_name)
           return
         end
 
-        r.reply("Here are the cards in #{list.name}:\n\n" +
+        r.reply(t('card.list', list_name: list_name) +
           list.cards.map { |card| "* #{card.name} - #{card.short_url}" }.join("\n")
         )
       end
 
       route(/^trello\s+lists$/i, :handle_lists, command: true, help: {
-        "trello lists" => "Show all lists on your board"
+        t('help.lists.syntax') => t('help.lists.desc')
       })
       def handle_lists(r)
-        r.reply("Here are all the lists on your board:\n\n" +
+        r.reply(t('list.list') +
           lists.keys.map { |list_name| "* #{list_name}" }.join("\n")
         )
       end
